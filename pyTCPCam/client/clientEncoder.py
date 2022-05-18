13a8c5fee9023b171e61bd1452ee8a766a853a6c
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-05-18 10:57:03 +0800

    pyTCPCam Week 2: TCP Client and Threading

--- pyTCPCam/client/clientEncoder.py
+++ pyTCPCam/client/clientEncoder.py
@@ -1,3 +1,26 @@
+import simplejpeg
+from threading import Thread
+
+#TODO in addition, reduce the size of the jpeg before sending
 class ClientEncoder:
     def __init__(self, stream):
-        
+        self.stream = stream
+    
+    def start(self):
+        Thread(target=self.encode, args=()).start()
+        return self
+
+    def encode(self):
+        while True:
+            if self.completed:
+                return
+
+            frame = self.stream.getFrame()
+            if (frame == None): continue #skip the frame if it is still being prepared
+            self.encodedFrame = simplejpeg.encode_jpeg(frame, quality=40, colorspace='BGR') #40 has minimal difference to the eyes
+    
+    def getEncodedFrame(self):
+        return self.encodedFrame
+    
+    def complete(self):
+        self.completed = True
--- pyTCPCam/client/clientTCP.py
+++ pyTCPCam/client/clientTCP.py
@@ -2,7 +2,9 @@ import cv2
 import imagezmq
 import simplejpeg
 import imutils
+from pyTCPCam.client.clientEncoder import ClientEncoder
 from pyTCPCam.client.clientStream import ClientStream
+from threading import Thread
 
 #set to the HOST:PORT to connect to
 HOST = "192.168.1.53"
@@ -12,7 +14,7 @@ PORT = 8100
 NAME = "two"
 
 #ImageZMQ sender init
-sender = imagezmq.ImageSender(connect_to=f"tcp://{HOST}:{PORT}")
+#sender = imagezmq.ImageSender(connect_to=f"tcp://{HOST}:{PORT}")
 
 #connects to a TCP server to send image data over the network
 #for optimization:
@@ -22,38 +24,59 @@ sender = imagezmq.ImageSender(connect_to=f"tcp://{HOST}:{PORT}")
 #in addition, only sends the latest frame that could be processed to the server
 #TODO in addition, move to publisher to remove blocking latency
 #TODO in addition, server shall process each stream in a separate thread to reduce latency
-#TODO in addition, client shall process each stream in a separate thread to reduce latency
+#in addition, client shall process each stream in a separate thread to reduce latency
 #in addition, FPS counter
 
 #starts the camera and sends data received from clientStream
 #clientStream is started on a separate thread for each camera
+#clientEncoder will encode and do optimizations to the image before sending
+#tcp client will always try to send the latest frame encoded
+#TODO due to race conditions, tcp client may try to 
 def main():
     cam0 = ClientStream(0).start()
+    cam0Encoder = ClientEncoder(cam0).start()
+    tcpClient0 = ClientTCP("Cam 0", cam0Encoder, HOST, PORT).start()
  
-    #TODO move this into another thread
+    #TODO test first, then move this into another thread
     while(True):
-        frame = cam0.getFrame()
-        jpg_buffer = simplejpeg.encode_jpeg(frame, quality=40, colorspace='BGR') #40 has minimal difference to the eyes
-
-        #call send through server
-        sendData(jpg_buffer)
-
         #DEBUG PREVIEW can remove this if client doesnt need to preview
-        cv2.imshow('clientFrame', frame) 
+        if (cam0.getFrame() == None): continue
+        cv2.imshow('clientFrame', cam0.getFrame()) 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     #DEBUG PREVIEW can be removed if client doesnt need to preview
     cv2.destroyAllWindows()
     cam0.complete()
-    
-#try sending the frame over the network
-def sendData(frame):
-    try:
-        sender.send_jpg(NAME, frame)
-    except Exception as e:
-        print(e)
+    cam0Encoder.complete()
+    tcpClient0.complete()
 
 #run as main things
 if __name__ == '__main__':
     main()
+
+class ClientTCP:
+    def __init__(self, name, encoder, host, port):
+        self.encoder = encoder
+        self.sender = imagezmq.ImageSender(connect_to=f"tcp://{host}:{port}")
+        self.name = name
+    
+    def start(self):
+        Thread(target=self.sendData, args=()).start()
+        return self
+    
+    def sendData(self):
+        while True:
+            if self.completed:
+                return
+
+            frame = self.encoder.getEncodedFrame()
+            if (frame == None): continue #skip the frame if it is still being prepared
+            
+            try:
+                self.sender.send_jpg(self.name, frame)
+            except Exception as e:
+                print(e)
+    
+    def complete(self):
+        self.completed = True
