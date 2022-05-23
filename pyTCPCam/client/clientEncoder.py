620c9de1d796a035f54d436c3f8e81cadf194723
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-05-23 13:09:11 +0800

    pyTCPCam Week 3: apply data class to encoder

--- pyTCPCam/client/clientEncoder.py
+++ pyTCPCam/client/clientEncoder.py
@@ -1,5 +1,9 @@
+import time
+import jsonpickle
 import simplejpeg
 from threading import Thread
+
+from data.imageInference import ImageInference
 
 #TODO in addition, reduce the size of the jpeg before sending
 #TODO ClientEncoder to tell ClientTCP to send when ready
@@ -10,6 +14,7 @@ class ClientEncoder:
         self.quality = 40
         self.encodedFrame = simplejpeg.encode_jpeg(self.stream.getFrame(), self.quality, colorspace='BGR') #encode the first frame
         self.ready = True
+        self.imageInference = ImageInference(time.time())
     
     def start(self):
         Thread(target=self.encode, args=()).start()
@@ -22,7 +27,11 @@ class ClientEncoder:
                 return
 
             self.encodedFrame = simplejpeg.encode_jpeg(self.stream.getFrame(), self.quality, colorspace='BGR')
+            self.imageInference = ImageInference(time.time())
+            self.imageInference.setImageData(self.encodedFrame)
+            self.imageInference.addData({"temporray": "encoder"})
             self.ready = True
+            print(jsonpickle.encode(self.imageInference))
     
     #get the latest encoded frame
     def getEncodedFrame(self):
--- pyTCPCam/client/clientProcessor.py
+++ pyTCPCam/client/clientProcessor.py
@@ -1,7 +1,35 @@
+from threading import Thread
+
 #TODO AI stuff in another thread, before the ClientEncoder. AI to tell ClientEncoder to encode (probably with a variable) when ready
+
 class ClientProcessor():
-    def __init__(self):
+    def __init__(self, encoder):
+        self.completed = False
+        self.encoder = encoder
+        self.processedFrame = self.encoder.encodedFrame #TODO apply the AI here one time if possible
+        self.ready = False
         pass
     
+    def start(self):
+        Thread(target=self.process, args=()).start()
+        return self
+
+    #encode loop to encode the latest frame received
+    def process(self):
+        while True:
+            if self.completed:
+                return
+
+            self.processedFrame = None #TODO do some AI stuff here
+            self.ready = True
+
     def asInferenceObject(self): #can remove if not needed
         return {"x": 123, "y": 234, "confidence": 1, "inferred": "Person"}
+
+    #get the latest inferenced frame
+    def getProcessedFrame(self):
+        return self.processedFrame
+
+    #end the thread
+    def complete(self):
+        self.completed = True
