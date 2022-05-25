c01b2c70623f3861a356d6dd5c1d18704e64c865
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-05-25 14:25:08 +0800

    partial audio integration

---
+++ pyTCPCam/client/audio/audioProcessor.py
@@ -0,0 +1,80 @@
+from time import sleep
+import numpy as np
+from audioStream import AudioStream
+import yamnet.params as yamnet_params
+import yamnet.yamnet as yamnet_model
+import yamnet.metadata as metadata
+from operator import itemgetter
+from threading import Thread
+
+class AudioProcessor():
+    def __init__(self, fileName, listenWindow, audioStream):
+        self.params = yamnet_params.Params()
+        self.yamnet = yamnet_model.yamnet_frames_model(self.params)
+        self.yamnet.load_weights(fileName)
+        self.yamnet_classes = np.array([x['name'] for x in metadata.CAT_META])
+        self.inferredResults = []
+        self.listenWindow = listenWindow
+        self.audioStream = audioStream
+        self.completed = False
+
+    def infer(self):
+        audio_data = np.transpose(self.audioStream.getFrames())
+        if len(audio_data.shape) > 1:
+            audio_data = np.mean(audio_data,axis=1)
+
+        scores,embeddings,spectrogram = self.yamnet(audio_data)
+
+        top_N = 5
+        mean_scores = np.mean(scores,axis=0)
+        top_class_indices = np.argsort(mean_scores)[::-1][:top_N]
+
+        results = []
+        total_scores = 0
+        for i in top_class_indices:
+            pred_class = self.yamnet_classes[i].replace("'","")
+            pred = (pred_class,mean_scores[i])
+            total_scores += mean_scores[i]
+            results.append(pred)
+
+        unknown_class_score = 1.0 - total_scores
+        results.append(("unknown",unknown_class_score))
+        
+
+        self.inferredResults = sorted(results,key=itemgetter(1),reverse=True)
+
+    def start(self):
+        Thread(target=self.listen, args=()).start()
+        return self
+    
+    def complete(self):
+        self.completed = True
+    
+    def listen(self):
+        """
+        Function that listens from the latest <listen_window> amount of seconds from
+        the microphone and prints the results to console. Used for unit testing.
+
+        Args:
+            microphone (_type_): _description_
+            listen_window (int, optional): _description_. Defaults to 1.
+        """
+        while True:
+            if self.completed:
+                return
+            
+            if (not self.audioStream.ready):
+                continue
+
+            print("Listening")
+            self.infer()
+            self.audioStream.ready = False
+            print(self.inferredResults)
+
+
+if __name__ == "__main__":
+    stream = AudioStream(16000, "numpy_tf", 1).start()
+    listener = AudioProcessor('yamnet.h5', 1, stream).start()
+    sleep(10) #temporary to stop
+    stream.complete()
+    listener.complete()
--- pyTCPCam/client/audio/audioStream.py
+++ pyTCPCam/client/audio/audioStream.py
@@ -4,44 +4,53 @@ import pyaudio
 import wave
 import logging
 import numpy as np
+from threading import Thread
 
-class Microphone:
+class AudioStream:
     """Class representing the microphone device.
     """
 
     class FormatNotSupportedError(Exception):
         pass
 
-
-    def __init__(self,chunksize):
+    def __init__(self, chunksize, listenType="raw", listenWindow=1):
         """[summary]
-
         Args:
             chunksize ([type]): [description]
         """
+        self.completed = False
+        self.SAMPLERATE = 16000
+        self.CHUNK = chunksize
+        self.FORMAT = pyaudio.paInt16
+        self.frames = []
+        self.ready = False
+        self.audio = pyaudio.PyAudio()
+        self.listenType = listenType
+        self.listenWindow = listenWindow
+        self.completed = False
+
         try:
             self.CHANNELS = 2
-            self.SAMPLERATE = 16000
-            self.CHUNK = chunksize
-            self.FORMAT = pyaudio.paInt16
-            self.frames = []
-            self.audio = pyaudio.PyAudio()
             self.stream = self.audio.open(format=self.FORMAT,channels=self.CHANNELS,rate=self.SAMPLERATE,input=True,frames_per_buffer=self.CHUNK)
             logging.info("Microphone successfully started")
 
         except Exception as e:
             self.CHANNELS = 1
-            self.SAMPLERATE = 16000
-            self.CHUNK = chunksize
-            self.FORMAT = pyaudio.paInt16
-            self.frames = []
-            self.audio = pyaudio.PyAudio()
             self.stream = self.audio.open(format=self.FORMAT,channels=self.CHANNELS,rate=self.SAMPLERATE,input=True,frames_per_buffer=self.CHUNK)
             logging.info("Switching to mono channel microphone")
             logging.info("Microphone successfully started")
+    
+    def start(self):
+        Thread(target=self.getAudio, args=()).start()
+        return self
 
+    def getFrames(self):
+        return self.frames
 
-    def get_audio(self,listen_window,blocking=True,fmt="raw",filename="data"):
+    def complete(self):
+        self.completed = True
+
+    def getAudio(self,filename="data"):
         """[summary]
 
         Args:
@@ -56,33 +65,27 @@ class Microphone:
         Returns:
             [type]: [description]
         """
-        
-        
-        if blocking:
-            if fmt == "numpy":
-                return self.recorder_numpy(listen_window)
+        while True:
+            if self.completed:
+                return
 
-            if fmt == "numpy_tf":
-                return self.recorder_numpy_tf(listen_window)
-            if fmt == "raw":
-                raw_data = self.recorder(listen_window)
-                return raw_data
-            elif fmt == "wav":
-                raw_data = self.recorder(listen_window)
+            if self.listenType == "numpy":
+                self.recorder_numpy(self.listenWindow)
+            elif self.listenType == "numpy_tf":
+                self.recorder_numpy_tf(self.listenWindow)
+            elif self.listenType == "raw":
+                self.recorder(self.listenWindow)
+
+            elif self.listenType == "wav": #TODO check how to run this in a thread
+                raw_data = self.recorder(self.listenWindow)
                 with wave.open(filename + ".wav","wb") as wavefile:
                     wavefile.setframerate(self.SAMPLERATE)
                     wavefile.setnchannels(self.CHANNELS)
                     wavefile.setsampwidth(self.audio.get_sample_size(self.FORMAT))
                     wavefile.writeframes(b''.join(raw_data))
-                    return filename + ".wav"
+                    #return filename + ".wav"
             else:
                 raise self.FormatNotSupportedError("Supported Formats are 'raw' and 'wav'")
-
-
-        else:
-            #TODO: Supoort Non-blocking mode with multithread
-            pass
-
 
     def recorder(self,listen_window):
         """[summary]
@@ -94,8 +97,7 @@ class Microphone:
         for i in range(0,int(self.SAMPLERATE/self.CHUNK * listen_window)):
             data = self.stream.read(self.CHUNK)
             self.frames.append(data)
-
-        return self.frames
+        self.ready = True
 
     def recorder_numpy(self,listen_window):
         """[summary]
@@ -110,13 +112,12 @@ class Microphone:
             if self.CHANNELS == 2:
                 frame = np.stack((frame[1::2],frame[::2]),axis=0)
             if first:
-                frames_array = frame
+                self.frames = frame
                 first = False
             else:
-                frames_array = np.concatenate((frames_array,frame),axis=1)
-        frames_array = np.transpose(frames_array)
-        return frames_array
-
+                self.frames = np.concatenate((self.frames,frame),axis=1)
+        self.frames = np.transpose(self.frames)
+        self.ready = True
 
     def recorder_numpy_tf(self,listen_window):
 
@@ -128,18 +129,13 @@ class Microphone:
             if self.CHANNELS == 2:
                 frame = np.stack((frame[1::2],frame[::2]),axis=0)
             if first:
-                frames_array = frame
+                self.frames = frame
                 first = False
             else:
-                frames_array = np.concatenate((frames_array,frame),axis=1)
-        
+                self.frames = np.concatenate((self.frames,frame),axis=1)
+        self.ready = True
 
-        
-
-        return frames_array
-
-
-    def get_all_numpy_tf(self,prev_audio_frame_array):
+    def get_all_numpy_tf(self,prev_audio_frame_array): #TODO check threading
         """
         """
         audio_data = self.stream.read(self.stream.get_read_available())
@@ -154,6 +150,4 @@ class Microphone:
         else:
             audio_frame_array = np.concatenate((prev_audio_frame_array,audio_frame),axis=1)
         
-        
         return audio_frame_array
-
