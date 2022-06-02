bd801e0fdf49166d144aaabbbeeeaef1c24dbb66
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-06-02 16:25:45 +0800

    debug 33

--- pyTCPCam/client/client.py
+++ pyTCPCam/client/client.py
@@ -49,7 +49,7 @@ class Client():
         self.videoEncoder = VideoEncoder(self.videoProcessor, self.tcp).start()
 
         #DEBUG PREVIEW can remove this if client doesnt need to preview
-        #self.videoDebug = self.videoProcessor.startDebug()
+        self.videoDebug = self.videoProcessor.startDebug()
 
         # while (flag.value):
         #     pass
