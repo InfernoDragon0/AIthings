c748c2857d93dee2311d5bf094d15bd260e1a267
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-06-09 15:20:47 +0800

    add debug rtsp

--- pyTCPCam/client/testOriginal.py
+++ pyTCPCam/client/testOriginal.py
@@ -1,7 +1,7 @@
 import cv2
 
 if __name__ == "__main__":
-    stream = cv2.VideoCapture(0)
+    stream = cv2.VideoCapture("rtsp://admin:amarisipc1@192.168.1.64:554/Streaming/Channels/101/")
     while True:
         (available, frame) = stream.read()
         if available:
