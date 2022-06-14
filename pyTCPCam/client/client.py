7feef0785f579db302d2fee5abe50a5484869b4a
Jia Cheng <dazzed17@gmail.com>
2022-06-14 11:17:20 +0800

    Adjusted to use local cam with no audio, no server requirements

--- pyTCPCam/client/client.py
+++ pyTCPCam/client/client.py
@@ -1,6 +1,6 @@
 from time import sleep
-from audio.audioStream import AudioStream
-from audio.audioProcessor import AudioProcessor
+#from audio.audioStream import AudioStream
+#from audio.audioProcessor import AudioProcessor
 from data.imageInference import ImageInference
 from video.videoStream import VideoStream
 from video.videoEncoder import VideoEncoder
@@ -43,10 +43,10 @@ class Client():
 
     def runCam(self, cameraId, flag):
         #init video stream
-        self.tcp = ClientTCP(f"Cam {cameraId}", HOST, PORT)
+        #self.tcp = ClientTCP(f"Cam {cameraId}", HOST, PORT)
         self.videoStream = VideoStream(cameraId).start()
         self.videoProcessor = VideoProcessor(self.videoStream).start()
-        self.videoEncoder = VideoEncoder(self.videoProcessor, self.tcp).start()
+        #self.videoEncoder = VideoEncoder(self.videoProcessor, self.tcp).start()
 
         #DEBUG PREVIEW can remove this if client doesnt need to preview
         self.videoDebug = self.videoProcessor.startDebug()
@@ -89,11 +89,11 @@ class AudioClient():
 #run main code
 def main():
     #run as many clients as you want as long as it is one camera per Client object
-    #cam0 = Client(0) #can swap in with a .mp4 file to test without camera
+    cam0 = Client(0) #can swap in with a .mp4 file to test without camera
     #cam0 = Client("/dev/video0")
     #cam0 = Client("v4l2src device=/dev/video0 ! video/x-raw,format=YUY2,width=640,height=480,framerate=30/1 ! videoconvert ! video/x-raw,format=BGR ! appsink")
-    cam0 = Client("rtsp://admin:amarisipc1@192.168.1.64:554/Streaming/Channels/101/")
-    audio0 = AudioClient(11)
+    #cam0 = Client("rtsp://admin:amarisipc1@192.168.1.64:554/Streaming/Channels/101/")
+    #audio0 = AudioClient(11)
 
 
             
@@ -104,7 +104,7 @@ def main():
 
     sleep(200)
     cam0.stop()
-    audio0.stop()
+    #audio0.stop()
     #sys.exit(0)
 
 #run main
