b5b39a94bcbf633d8ee94e40a300955eebe07153
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-06-15 11:53:17 +0800

    testclient for rtsp

--- pyTCPCam/client/testClient.py
+++ pyTCPCam/client/testClient.py
@@ -30,7 +30,8 @@ if __name__ == '__main__':
 
     imageModel = VideoProcessor(camQueue, encQueue, resultQueue).startAsProcess()
     #start each video stream as a separate process
-    videoStream0 = VideoStream("vlc.mp4", 60, camQueue, False).startAsProcess()
+    #videoStream0 = VideoStream("vlc.mp4", 60, camQueue, False).startAsProcess()
+    videoStream0 = VideoStream("rtsp://admin:amarisipc1@192.168.1.64:554/Streaming/Channels/101/", 60, camQueue, True).startAsProcess()
 
     #here the main process will encode and send thru tcp
     tcp = ClientTCP("Integrated Inference Client", "127.0.0.1", 8100)
