8d94e6333301d4af205e3619b08188601067b754
Xu Min <xmin23@gmail.com>
2022-06-22 14:44:09 +0800

    Data is recorded in output file

--- pyTCPCam/client/server.py
+++ pyTCPCam/client/server.py
@@ -10,6 +10,7 @@ import time
 import csv
 import numpy as np
 import tensorflow as tf
+import datetime 
 HOST = "0.0.0.0"
 PORT = 2004
 audio_list = []
@@ -34,13 +35,14 @@ def multi_threaded_client(connection):
         data = connection.recv(64000)
         try:
             dataPickle = jsonpickle.decode(data)
-
+            f = open("output.txt","a")
             #global inference data check
             if dataPickle.inferredData is None or len(dataPickle.inferredData) == 0: 
-                print(f"{dataPickle.packetType}: No data available for inference")
+                print(f"{dataPickle.packetType}: No object of interest in image")
                 continue
             if dataPickle.packetType == 'Audio':
-                print(f"Audio data received: {dataPickle.inferredData} at time {dataPickle.timestamp}")
+                packetTime_audio = datetime.datetime.now() 
+                print(f"Audio data received: {dataPickle.inferredData} at time {packetTime_audio}")
                 #Get audio name
                 audio_name = audio_class_names("yamnet_class_map.csv")
                 #Check audio sound is human and check for value that is great or equal to 10
@@ -49,13 +51,15 @@ def multi_threaded_client(connection):
                         if audio_name[val] == dataPickle.inferredData[i]['name']:
                             if float(dataPickle.inferredData[i]['value']) >= 0.5:       
                                 print("Human sound detected")
+                                print("Audio data received:" + dataPickle.inferredData[i]['name'] +" and the value is "+ dataPickle.inferredData[i]['value'] +" at time " +str(packetTime_audio),file=f)
                                 flag_audio += 1           
                                 break
                         break
-                print("Continue with Image")
             elif dataPickle.packetType == 'Image':
                 face_counter = len(dataPickle.inferredData)
-                print(f"Number of faces received: {len(dataPickle.inferredData)} at time {dataPickle.timestamp}")
+                packetTime_image = datetime.datetime.now()
+
+                print(f"Number of faces received: {len(dataPickle.inferredData)} at time {packetTime_image}")
                 if dataPickle.imageData is not None:
                     decodedImage = simplejpeg.decode_jpeg(dataPickle.imageData, colorspace='BGR')
                     cv2.imshow("server", decodedImage)
@@ -63,21 +67,26 @@ def multi_threaded_client(connection):
                 #Check for faces
                 if len(dataPickle.inferredData) >= 1:
                     print("Human is detected")
+                    print("Number of faces received:" +len(dataPickle.inferredData) +" at time " +str(packetTime_image))
+
                     flag_image += 1
             elif dataPickle.packetType == 'Sensor':
-                print(f"Sensor data received: {dataPickle.inferredData} at time {dataPickle.timestamp}")
+                packetTime_sensor = datetime.datetime.now()
+                print(f"Sensor data received: {dataPickle.inferredData} at time {packetTime_sensor}")
                 #Sensor check
                 if dataPickle.inferredData == 1:
+                    print("Sensor data received: "+ dataPickle.inferredData+ " at time "+str(packetTime_sensor))
                     flag_sensor += 1
             else:
                 print(f"New data type found: {dataPickle.packetType}")
             #check flag
-            if flag_audio == 1 and flag_image== 1 or flag_sensor== 1 and flag_audio == 1 or flag_image == 1 and flag_sensor == 1:
+            if flag_audio == 1 and flag_image== 1 or flag_sensor== 1 and flag_audio == 1 or flag_image == 1 and flag_sensor == 1 or flag_audio == 1 and flag_image == 1 and flag_sensor == 1:
                 #alert will pop up
                 if flag_image == 1:
                     messagebox.showerror("Alert", "Date of Alert: %s/%s/%s" % (e.day, e.month, e.year) + "\nTime of Alert: %s:%s:%s" % (e.hour, e.minute, e.second)+ "\nNumber of people at location now: "+ face_counter)
                 else:
                     messagebox.showerror("Alert", "Date of Alert: %s/%s/%s" % (e.day, e.month, e.year) + "\nTime of Alert: %s:%s:%s" % (e.hour, e.minute, e.second)+ "\nNumber of people at location now: not found \nAudio and Sensor detected people")
+            f.close()
         except Exception as e:
             print(f"Error reading data json {e}")
         
@@ -93,7 +102,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     print("Server running")
-
     while True:
         conn, addr = s.accept()
         Thread(target=multi_threaded_client, args=(conn, )).start()
