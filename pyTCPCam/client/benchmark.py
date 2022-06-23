6b1ec76bef4674e166fdf649ecbe50243578848c
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-06-23 17:40:14 +0800

    add benchmark time

--- pyTCPCam/client/benchmark.py
+++ pyTCPCam/client/benchmark.py
@@ -13,8 +13,8 @@ from video.videoStream import VideoStream
 #       - Camera Capture Time: 0.0000 seconds per frame
 #       - Inference Time: 0.0000 seconds per frame
 #   - FPS: 60
-#       - Camera Capture Time: 0.0000 seconds per frame
-#       - Inference Time: 0.0000 seconds per frame
+#       - Camera Capture Time: 0.02 - 0.07 seconds per frame
+#       - Inference Time: 0.20 - 0.30 seconds per frame
 # 1920 x 1080
 #   - FPS: 20
 #       - Camera Capture Time: 0.0000 seconds per frame
@@ -35,17 +35,6 @@ from video.videoStream import VideoStream
 #   - FPS: 60
 #       - Camera Capture Time: 0.0000 seconds per frame
 #       - Inference Time: 0.0000 seconds per frame
-# 3840 x 2160
-#   - FPS: 20
-#       - Camera Capture Time: 0.0000 seconds per frame
-#       - Inference Time: 0.0000 seconds per frame
-#   - FPS: 40
-#       - Camera Capture Time: 0.0000 seconds per frame
-#       - Inference Time: 0.0000 seconds per frame
-#   - FPS: 60
-#       - Camera Capture Time: 0.0000 seconds per frame
-#       - Inference Time: 0.0000 seconds per frame
-
 ###############################
 
 if __name__ == '__main__':
@@ -62,5 +51,5 @@ if __name__ == '__main__':
     #videoStream0 = VideoStream(f"rtspsrc location={rtspSrc} latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=BGRx, width=1920, height=1080 ! videorate ! 'video/x-raw(memory:NVMM),framerate=20/1'", 20, camQueue, True).startAsProcess()
     #videoStream0 = VideoStream(f"rtspsrc location={rtspSrc} latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=BGRx, width=2048, height=1536 ! videorate ! 'video/x-raw(memory:NVMM),framerate=20/1'", 20, camQueue, True).startAsProcess()
     #videoStream0 = VideoStream(f"rtspsrc location={rtspSrc} latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=BGRx, width=3840, height=2160 ! videorate ! 'video/x-raw(memory:NVMM),framerate=20/1'", 20, camQueue, True).startAsProcess()
-    #videoStream0 = VideoStream(f"rtspsrc location={rtspSrc} latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=BGRx, width=1280, height=720 ! videoconvert ! video/x-raw,format=BGR ! appsink", 60, camQueue, True).startAsProcess()
+    videoStream0 = VideoStream(f"rtspsrc location={rtspSrc} latency=0 ! rtph264depay ! h264parse ! nvv4l2decoder ! nvvidconv ! video/x-raw, format=BGRx, width=1280, height=720 ! videoconvert ! video/x-raw,format=BGR ! appsink", 60, camQueue, True).startAsProcess()
     videoStream0 = VideoStream("rtsp://admin:amarisipc1@10.1.1.10:554/Streaming/Channels/101/", 60, camQueue, True).startAsProcess()
