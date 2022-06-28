bf6b2918fa75546553c548a3f7966b41c37e61e5
muhaimin97 <muhaimin271997@gmail.com>
2022-06-28 12:54:48 +0800

    changed ip address of server

--- pyTCPCam/client/testClient.py
+++ pyTCPCam/client/testClient.py
@@ -47,8 +47,8 @@ if __name__ == '__main__':
     #videoStream0 = VideoStream("rtsp://admin:amarisipc1@192.168.1.64:554/Streaming/Channels/101/", 60, camQueue, True).startAsProcess()
 
     # #here the main process will encode and send thru tcp
-    tcp = ClientTCP("Integrated Audio/Video Client", "192.168.1.50", 2004)
-    tcp2 = ClientTCP("Integrated Audio/Video Client 2", "192.168.1.50", 2004)
+    tcp = ClientTCP("Integrated Audio/Video Client", "192.168.31.207", 2004)
+    tcp2 = ClientTCP("Integrated Audio/Video Client 2", "192.168.31.207", 2004)
     #audio process
     audioStream0 = AudioStream(16000, audQueue, 11, "numpy_tf", 1).startAsProcess()
     audioProcessor = AudioProcessor('yamnet.tflite', 1, audQueue, tcp2).startAsProcess()
