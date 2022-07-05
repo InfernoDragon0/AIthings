d6aa50b4a2b0d1d803d50474ebbbd311fbcce498
muhaimin97 <muhaimin271997@gmail.com>
2022-07-05 22:02:09 +0800

    added inferenceType property to ImageInference class

--- pyTCPCam/client/data/imageInference.py
+++ pyTCPCam/client/data/imageInference.py
@@ -4,6 +4,7 @@ from data.baseInference import BaseInference
 class ImageInference(BaseInference):
     def __init__(self):
         super().__init__()
+        self.inferenceType = "dog"
         self.packetType = "Image"
         self.streamImage = False #set to true if want to send the image as well
         self.imageData = None
