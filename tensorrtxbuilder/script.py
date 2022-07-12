8256998a3b54df52ae20c34dd3eeb0d9630260cd
Jia Cheng <dazzed17@gmail.com>
2022-07-12 14:25:22 +0800

    added build folder creation

--- tensorrtxbuilder/script.py
+++ tensorrtxbuilder/script.py
@@ -13,6 +13,7 @@ PATH_YOLOV5INF = "/yolov5_inferenceonly/"
 FILE_GENWTS = "gen_wts.py"
 FILE_WTS = "atasv3.wts"
 
+CMD_MKBUILDFD = ["mkdir", "build"]
 
 #Script requires at least python 3 for the input
 print("Starting tensorrtxbuilder...")
@@ -55,6 +56,19 @@ except Exception as e:
     exit()
 
 # CREATING BUILD FOLDER IN YOLOV5 FOLDER 
+try:
+    print("Creating /build folder in yolov5 folder...")
+    output = subprocess.run(CMD_MKBUILDFD, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=DIR_CURRENT+PATH_YOLOV5)
+    if (output.returncode == 0):
+        print("build folder created successfully!\n")
+    else:
+            print("build folder creation error!")
+            print("printing full output...")
+            print(output)
+            print("exiting...")
+            exit()
+except Exception as e:
+    print(e)
 
 # COPYING .WTS FILE OVER TO BUILD FOLDER IN YOLOV5
 
