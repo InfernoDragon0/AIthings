9a2dacdd629a9fc373b137e792784d0c4f6eb1f5
WeijieProg <77503225+WeijieProg@users.noreply.github.com>
2022-07-21 14:10:14 +0800

    Update testClient.py
    Removed ToF, added vibrationStream.

--- pyTCPCam/client/testClient.py
+++ pyTCPCam/client/testClient.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 
     # sensor process
     #sensorStream = SensorStream(tcp).startAsProcess()
-    tofStream = ToFStream(tcp).startAsProcess()
+    vibrationStream = vibrationStream(tcp).startAsProcess()
     #ultrasonicStream = UltrasonicStream(tcp).startAsProcess()
 
     # #encoder and TCP
