52473622a8daf5172e58d14b2be1b1a08e6dec34
WeijieProg <77503225+WeijieProg@users.noreply.github.com>
2022-07-21 11:12:17 +0800

    Add files via upload

---
+++ pyTCPCam/client/sensor/ToFStream.py
@@ -0,0 +1,73 @@
+import multiprocessing
+import Jetson.GPIO as GPIO
+import VL53L1X
+
+from data.sensorInference import SensorInference
+
+
+class SensorStream:
+
+    # init and read one frame
+    def __init__(self, tcp):
+        self.tof = VL53L1X.VL53L1X(i2c_bus=1, i2c_address=0x29)
+        self.distance = 0
+        self.benchmark = 0
+
+    def startAsProcess(self):
+        print("Sensor Stream Process started")
+        self.tof.open()
+
+        # self.sensorProcess = multiprocessing.Process(target=self.distance)
+        self.sensorProcess = multiprocessing.Process(target=self.ToF, args=(self.tcp,))
+        self.sensorProcess.start()
+        return self
+
+    # Setup
+    def initBenchmark(self):
+        initVal = self.tof.get_distance()
+        cnt = 0
+        total = initVal
+
+        while True:
+            self.distance = self.tof.get_distance()
+            current = self.distance
+            prev = self.distance
+            if (current >= (initVal - (initVal * .05))) and (current <= (initVal + (initVal * .05))):
+                total += current
+                cnt += 1
+            else:
+                total -= prev
+                cnt -= 1
+
+            if cnt >= 5:
+                self.benchmark = total / cnt
+                break
+            elif cnt <= -5:
+                initVal = self.distance
+                cnt = 0
+                total = initVal
+
+            self.tof.stop_ranging()
+
+    # Get distance from ultrasonic sensor
+    def ToF(self, tcp):
+        while True:
+            self.sensorInference = SensorInference("real ToF sensor")
+
+            # 0 = Unchanged
+            # 1 = Short Range
+            # 2 = Medium Range
+            # 3 = Long Range
+            self.tof.start_ranging(3)  # Start ranging
+            self.distance = self.tof.get_distance()
+            print(str(self.distance) + 'mm')
+
+            if (self.distance >= (self.benchmark - (self.benchmark * .05))) and (self.distance <= (self.benchmark + (self.benchmark * .05))):
+                self.sensorInference.addData('0')
+                print(f"ToF sensor state: 0")
+            else:
+                self.sensorInference.addData('1')
+                print(f"ToF sensor state: 1")
+
+            tcp.sendData(self.sensorInference)
+            self.tof.stop_ranging()
